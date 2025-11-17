e392eee63f02346b8edf51ea5e8232aff0c7e768
Louin <134504671+LouinLee@users.noreply.github.com>
2025-11-18 00:14:12 +0800

    commit

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
     baseURL: process.env.REACT_APP_API_URL || "https://louininventory-890420967859.asia-southeast2.run.app/api",
     withCredentials: true,
 });
