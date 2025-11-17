720fbad5b58d709a9f1d29487a181cdc6951ebca
Louin <134504671+LouinLee@users.noreply.github.com>
2025-11-18 00:33:20 +0800

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "https://louininventory-890420967859.asia-southeast2.run.app/api",
+    baseURL: "https://louininventory-890420967859.asia-southeast2.run.app/api",
     withCredentials: true,
 });
 
