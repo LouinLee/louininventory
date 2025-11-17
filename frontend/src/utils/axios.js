ef0930bd0400af9eeec2fa061b1e5f71e20f4ff0
Louin <134504671+LouinLee@users.noreply.github.com>
2025-11-17 23:39:14 +0800

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+    // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+    baseURL: process.env.REACT_APP_API_URL || "http://louininventory-890420967859.asia-southeast2.run.app/api",
     withCredentials: true,
 });
 
