2ed26d0043edcf9833cc857a16dce1152f380802
Louin <134504671+LouinLee@users.noreply.github.com>
2025-11-18 14:08:09 +0800

    Update lock register

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
-router.get("/register", authController.registerForm);
-router.post("/register", authController.register);
+// router.get("/register", authController.registerForm);
+// router.post("/register", authController.register);
 router.get("/login", authController.loginForm);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
